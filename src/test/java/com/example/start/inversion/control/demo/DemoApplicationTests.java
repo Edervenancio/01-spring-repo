67a1b05c3f8d82daee6cb1bd066e5ca3ba5f2fb1
Eder venancio <ederpvenancio@gmail.com>
2023-07-10 20:14:04 -0300

    first commit

---
+++ src/test/java/com/example/start/inversion/control/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.start.inversion.control.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
