0de974e79a927d22272cbc9bfcd822f48a22845a
Eder venancio <ederpvenancio@gmail.com>
2023-07-10 22:19:58 -0300

    add configuration

--- src/main/java/com/example/start/inversion/control/demo/DemoApplication.java
+++ src/main/java/com/example/start/inversion/control/demo/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.example.start.inversion.control.demo;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- src/main/java/com/example/start/inversion/control/demo/rest/DemoController.java
+++ src/main/java/com/example/start/inversion/control/demo/rest/DemoController.java
@@ -1,2 +1,35 @@
-package com.example.start.inversion.control.demo.rest;public class DemoController {
+package com.example.start.inversion.control.demo.rest;
+
+
+import com.example.start.inversion.control.demo.common.Animal;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    private Animal myAnimal;
+
+
+    @Autowired
+    public DemoController(@Qualifier("fish") Animal theAnimal){
+        myAnimal = theAnimal;
+    }
+
+    @GetMapping("/sound")
+    public String sound(){
+        return myAnimal.sound();
+    }
+
+    @GetMapping("/movement")
+    public String moviment(){
+        return myAnimal.movement();
+    }
+
+    @GetMapping("/name")
+    public String name(){
+        return myAnimal.name();
+    }
 }
